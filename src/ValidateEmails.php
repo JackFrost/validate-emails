9c43789ba71f35c4157eeabdef02e8e4e074f3df
LRazumov <masdzen@gmail.com>
2024-03-06 20:34:41 +0000

    Realize check

--- src/ValidateEmails.php
+++ src/ValidateEmails.php
@@ -8,6 +8,23 @@ class ValidateEmails
 {
     public function check(array $emails): array
     {
-        return array_flip($emails);
+        return array_map(
+            function ($email): bool {
+                if (empty($email)) {
+                    return false;
+                }
+                list($user, $domain) = explode('@', $email);
+                $record = dns_get_record($domain, DNS_MX);
+                $record_host = $record[0]['host'] ?? '';
+                if ($record_host !== $domain) {
+                    return false;
+                }
+                elseif (empty($arr[0]['target'])) {
+                    return false;
+                }
+                return $arr[0]['target'];
+            },
+            filter_var($emails, FILTER_VALIDATE_EMAIL, FILTER_REQUIRE_ARRAY)
+        );
     }
 }
